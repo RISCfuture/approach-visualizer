f86beff620385e3840e7cf217e4978731cdd9fb4
Tim Morgan <github@timothymorgan.info>
2025-08-20 21:04:48 -0700

    Fix: Object.hasOwn is not a function

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,17 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import legacy from '@vitejs/plugin-legacy'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     process.env.NODE_ENV == 'development' ? vueDevTools({ launchEditor: 'rubymine' }) : false,
+    legacy({
+      targets: ['chrome >= 79', 'edge >= 79', 'safari >= 13', 'firefox >= 67'],
+      modernPolyfills: ['es.object.has-own']
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
