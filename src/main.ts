9170a7cbbcff3d3093b3926d9722d4d59283594f
Tim Morgan <github@timothymorgan.info>
2025-10-08 17:54:52 -0700

    Bugsnag -> Sentry

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import { createPinia } from 'pinia'
 import PrimeVue from 'primevue/config'
 import Aura from '@primeuix/themes/aura'
 import Tooltip from 'primevue/tooltip'
-import { initBugsnag } from './bugsnag'
+import * as Sentry from '@sentry/vue'
+import { createSentryPiniaPlugin } from '@sentry/vue'
 
 import 'normalize.css'
 import 'primeflex/primeflex.css'
@@ -14,8 +15,30 @@ import App from './App.vue'
 
 const app = createApp(App)
 
-initBugsnag(app)
-app.use(createPinia())
+const sentryDSN = import.meta.env.VITE_SENTRY_DSN
+Sentry.init({
+  app,
+  dsn: sentryDSN,
+  sendDefaultPii: true,
+  integrations: [
+    Sentry.vueIntegration({
+      tracingOptions: {
+        trackComponents: true,
+      },
+    }),
+    // Sentry.browserTracingIntegration({ router }),
+    Sentry.replayIntegration(),
+  ],
+  tracesSampleRate: 1.0,
+  enableLogs: true,
+  replaysSessionSampleRate: 0.1,
+  replaysOnErrorSampleRate: 1.0,
+})
+
+const pinia = createPinia()
+pinia.use(createSentryPiniaPlugin())
+app.use(pinia)
+
 app.use(PrimeVue, {
   theme: {
     preset: Aura,
